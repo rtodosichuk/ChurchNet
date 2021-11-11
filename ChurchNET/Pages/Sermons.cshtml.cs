06043eb3e76a6a52b9730688419c78559ee95f53
rtodosichuk <ron.todosichuk@gmail.com>
2021-11-11 11:23:10 -0500

    live steam - latest changes

--- ChurchNET/Pages/Sermons.cshtml.cs
+++ ChurchNET/Pages/Sermons.cshtml.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Configuration;
 using Newtonsoft.Json;
 using System.Collections.Generic;
 using System.IO;
@@ -14,16 +15,25 @@ namespace ChurchNET.Pages
     public class SermonsModel : PageModel
     {
         private IWebHostEnvironment _env;
+        private IConfiguration _configuration;
 
         /// <summary>
         /// Sermons List that is sent to the view. 
         /// </summary>
-        [BindProperty]
-        public List<Sermon> Sermons { get; set; }
+        //[BindProperty]
+        //public List<Sermon> Sermons { get; set; }
 
-        public SermonsModel(IWebHostEnvironment env)
+        public string YouTubeEmdedLiveSteam { get; set; }
+        public string YouTubeDirectLiveSteam { get; set; }
+        public string YouTubeChannel { get; set; }
+
+        public SermonsModel(IWebHostEnvironment env, IConfiguration configuration)
         {
             _env = env;
+            _configuration = configuration;
+            YouTubeEmdedLiveSteam = _configuration["AppSettings:YouTube:EmdedLiveSteam"];
+            YouTubeDirectLiveSteam = _configuration["AppSettings:YouTube:DirectLiveSteam"];
+            YouTubeChannel = _configuration["AppSettings:YouTube:Channel"];
         }
 
         /// <summary>
@@ -31,8 +41,8 @@ namespace ChurchNET.Pages
         /// </summary>
         public void OnGet()
         {
-            var slist = System.IO.File.ReadAllText(Path.Combine(_env.ContentRootPath, "wwwroot/sermonlist.json"));
-            Sermons = JsonConvert.DeserializeObject<List<Sermon>>(slist);
+            //var slist = System.IO.File.ReadAllText(Path.Combine(_env.ContentRootPath, "wwwroot/sermonlist.json"));
+            //Sermons = JsonConvert.DeserializeObject<List<Sermon>>(slist);
         }
     }
 }
