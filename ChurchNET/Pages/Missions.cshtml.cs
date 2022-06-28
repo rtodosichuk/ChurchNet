41bb10fae7ced1f359d84e21a818c4b453a0f8fb
rtodosichuk <ron.todosichuk@gmail.com>
2022-06-27 20:42:43 -0400

    Adding Missions page

---
+++ ChurchNET/Pages/Missions.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ChurchNET.Pages
+{
+    public class MissionsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
