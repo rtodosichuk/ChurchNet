53187d2f230f85ed33822cd748038a0f70e3988b
rtodosichuk <ron.todosichuk@gmail.com>
2023-04-09 21:15:33 -0400

    Adding BestWeigh

---
+++ ChurchNET/Pages/events.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ChurchNET.Pages
+{
+    public class eventsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
